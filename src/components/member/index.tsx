88125e4392a77c1e966726715587862ef4722a42
guoyiwang <wanggy522@gmail.com>
2024-07-09 19:52:27 -0700

    add-R-generate-cross

--- src/components/member/index.tsx
+++ src/components/member/index.tsx
@@ -1,29 +1,39 @@
 import React, { FC } from "react";
 import "./style.scss";
-import Avatar from "@assets/avatar.png";
+import Avatar from "@assets/avatars/avatar.png";
+import coworkers from "@/components/namecross/crossInput.json";
 
 type memberProps = {
   name: string
-  avatar?: string
+  nameClue?: string
+  introduction?: string
+  avatarURL?: string
 }
 
-const Member: FC<memberProps> = ({ name, avatar }) => {
+const Member: FC<{name: string}> = ({ name }) => {
   if (!name) {
     return <div className="notice">
 
 ←  CLICK LEFT TO SELECT A MEMBER
     </div>
   }
+  const userDetails = getUserDetails(name)
+
+  function getUserDetails(name: string): memberProps | undefined{
+    return coworkers.find((user) => user.name === name)
+  }
+
   return (
     <div className="member">
       <div className="meta">
-        <img className="avatar" src={ avatar || Avatar } alt={ name } />
+        <img className="avatar" src={userDetails?.avatarURL || Avatar} alt={ name } />
         <p className="name">{ name }</p>
       </div>
       <div className="intro">
-        Blabala  balabala  balabala  babababaaa lalallalala balabala
+        {userDetails?.introduction || 
+        `Blabala  balabala  balabala  babababaaa lalallalala balabala
         balabala balabala
-        balabala balabala
+        balabala balabala`}
       </div>
     </div>
   );
