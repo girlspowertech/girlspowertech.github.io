0369726da7397b416996a99be182bf8898559a08
fish <fish404hsif@gmail.com>
2024-07-28 22:23:54 +0800

    add dark theme change

--- src/components/switch/index.tsx
+++ src/components/switch/index.tsx
@@ -1,16 +1,20 @@
-import React from "react";
 import './style.scss';
+import { useTheme } from "@/hooks/useTheme";
 
-const Switch = () => {
+export const Switch = () => {
+
+    const [theme, handleChange] = useTheme('light');
+
     return (
         <div className="container-switch">
             <span>Change Theme </span>
             <label className="switch">
-                <input type="checkbox" />
+                <input type="checkbox" onChange={handleChange} checked={theme === 'dark'} />
                 <span className="slider"></span>
             </label>
         </div>
     )
 }
+
 
 export default Switch;
